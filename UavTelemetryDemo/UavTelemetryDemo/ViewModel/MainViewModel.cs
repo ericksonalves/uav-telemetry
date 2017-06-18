c2e3d190c9e87c406b0a1f66b3bad556637b749b
Erickson Alves <erickson.higor@gmail.com>
2017-06-18 18:39:24 -0400

    Add sample RelayCommand
    Signed-off-by: Erickson Alves <erickson.higor@gmail.com>

--- UavTelemetryDemo/UavTelemetryDemo/ViewModel/MainViewModel.cs
+++ UavTelemetryDemo/UavTelemetryDemo/ViewModel/MainViewModel.cs
@@ -1,11 +1,21 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows;
 
 namespace UavTelemetryDemo.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        public RelayCommand UpdateCommand { get; set; }
+
         public MainViewModel()
         {
+            UpdateCommand = new RelayCommand(UpdateCommandAction);
+        }
+
+        private void UpdateCommandAction()
+        {
+            MessageBox.Show("Update Command Executed");
         }
     }
 }
