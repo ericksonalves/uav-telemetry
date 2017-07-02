df3faf94e7bfe80d97447b100affb9b5787fe897
Erickson Alves <erickson.higor@gmail.com>
2017-07-02 14:51:54 -0400

    Update demo UI
    Signed-off-by: Erickson Alves <erickson.higor@gmail.com>

---
+++ UavTelemetryDemo/UavTelemetryDemo/Converter/InvertedBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UavTelemetryDemo.Converter
+{
+    public class InvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+                return !(bool)value;
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- UavTelemetryDemo/UavTelemetryDemo/ViewModel/MainViewModel.cs
+++ UavTelemetryDemo/UavTelemetryDemo/ViewModel/MainViewModel.cs
@@ -6,6 +6,55 @@ namespace UavTelemetryDemo.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        private bool sendToLocalhost;
+        public bool SendToLocalhost
+        {
+            get { return sendToLocalhost; }
+            set { Set(() => SendToLocalhost, ref sendToLocalhost, value); }
+        }
+
+        private string altitude;
+        public string Altitude
+        {
+            get { return altitude; }
+            set { Set(() => Altitude, ref altitude, value); }
+        }
+
+        private string battery;
+        public string Battery
+        {
+            get { return battery; }
+            set { Set(() => Battery, ref battery, value); }
+        }
+
+        private string ip;
+        public string Ip
+        {
+            get { return ip; }
+            set { Set(() => Ip, ref ip, value); }
+        }
+
+        private string latitude;
+        public string Latitude
+        {
+            get { return latitude; }
+            set { Set(() => Latitude, ref latitude, value); }
+        }
+
+        private string longitude;
+        public string Longitude
+        {
+            get { return longitude; }
+            set { Set(() => Longitude, ref longitude, value); }
+        }
+
+        private string speed;
+        public string Speed
+        {
+            get { return speed; }
+            set { Set(() => Speed, ref speed, value); }
+        }
+
         public RelayCommand UpdateCommand { get; set; }
 
         public MainViewModel()
